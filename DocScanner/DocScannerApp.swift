e055967f28067385bf51100f79eaae2effb214d3
Jacob McCloud <jmccloud@uwm.com>
2025-02-24 08:14:20 -0500

    Initial Commit

---
+++ DocScanner/DocScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocScannerApp.swift
+//  DocScanner
+//
+//  Created by Jacob McCloud on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct DocScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
